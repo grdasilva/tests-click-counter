4b487aff386d8d732931f7c0c155743d5bf6a1fa
Guilherme <guirod961@gmail.com>
2021-06-07 18:23:02 -0300

    set up enzyme, writing tests and creating notes archive

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+
+});
+
+test('renders increment button', () => {
+  
+});
+
+test('renders counter display', () => {
+  
+});
+
+test('counter display starts at 0', () => {
+  
+});
+
+test('clicking button increments the counter', () => {
+  
 });
