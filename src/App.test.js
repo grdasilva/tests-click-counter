ffc073f3baab4bac1247369823e33771cc22a7a7
Guilherme <guirod961@gmail.com>
2021-06-08 17:36:34 -0300

    starting the eject npm

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,9 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('renders without error', () => {
-
+    const wrapper = shallow(<App />);
+    const appComponent = wrapper.find("[data-test='component-app']"); // find the data-test attribute on app
+    expect(appComponent.length).toBe(1); // expect an result equal to 1 to success, another result will throw a new error
 });
 
 test('renders increment button', () => {
