71041ea90c76eb8675d781e764aa4a43fee89e6f
Guilherme <guirod961@gmail.com>
2021-06-24 18:45:52 -0300

    writing tests of event action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = React.useState(0);
+
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently</h1>
-      <button data-test="increment-btn">Increment counter</button>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span> 
+      </h1>
+      <button data-test="increment-btn" onClick={() => setCount(count + 1)}>Increment counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -36,6 +36,22 @@ test("renders counter display", () => {
   expect(counterDisplay.length).toBe(1);
 });
 
-test("counter display starts at 0", () => {});
+test("counter display starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text(); // .text() returns an string
+  expect(count).toBe("0");
+});
 
-test("clicking button increments the counter", () => {});
+test("clicking button increments the counter", () => {
+  const wrapper = setup();
+  
+  // find the button
+  const button = findByTestAttr(wrapper, "increment-btn");
+
+  // click the button
+  button.simulate('click'); // .simulate() makes an simulation of an event
+
+  // find the display, and test that number has been incremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
